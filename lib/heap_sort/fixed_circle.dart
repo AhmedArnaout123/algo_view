8006ff58ec91da41105c944f0b49a5c1283f3135
ahmed_arnaout <ahmed.arnaout123@gmail.com>
2020-09-22 11:35:22 +0300

    Update FixedCircle widget so that it takes the border from it's parent

--- lib/heap_sort/fixed_circle.dart
+++ lib/heap_sort/fixed_circle.dart
@@ -4,20 +4,24 @@ class FixedCircle extends StatelessWidget {
   final Offset center;
   final double radius;
   final Widget child;
+  final Border border;
 
-  const FixedCircle({Key key, this.center, this.radius, this.child})
+  const FixedCircle(
+      {Key key, this.center, this.radius, this.child, this.border})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Transform.translate(
-        offset: center - Offset(radius, radius),
-        child: Container(
-          height: 2 * radius,
-          width: 2 * radius,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(radius),
-              border: Border.all(width: 0.5)),
-          child: child,
-        ));
+      offset: center - Offset(radius, radius),
+      child: Container(
+        height: 2 * radius,
+        width: 2 * radius,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius),
+          border: border,
+        ),
+        child: child,
+      ),
+    );
   }
 }
