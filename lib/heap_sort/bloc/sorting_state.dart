af20d9aed784583254f2dc2e0c4d5d2423c8d17d
AhmedArnaout123 <ahmed.arnaout123@gmail.com>
2020-09-28 22:38:46 +0300

    Add needed properties to some  sorting bloc states classes

--- lib/heap_sort/bloc/sorting_state.dart
+++ lib/heap_sort/bloc/sorting_state.dart
@@ -7,8 +7,20 @@ class SortingInitial extends SortingState {}
 
 class StableSortingState extends SortingState {}
 
-class SwapingItemsSortingState extends SortingState {}
+class SwapingItemsSortingState extends SortingState {
+  final int index1, index2;
 
-class ComparingItemsInProgressSortingState extends SortingState {}
+  SwapingItemsSortingState(this.index1, this.index2);
+}
 
-class ComparingItemsIsDoneSortingState extends SortingState {}
+class ComparingItemsInProgressSortingState extends SortingState {
+  final int index1, index2;
+
+  ComparingItemsInProgressSortingState(this.index1, this.index2);
+}
+
+class ComparingItemsIsDoneSortingState extends SortingState {
+  final int index1, index2;
+
+  ComparingItemsIsDoneSortingState(this.index1, this.index2);
+}
