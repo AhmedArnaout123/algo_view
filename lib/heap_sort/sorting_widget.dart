8b517e5509b4365f08ff2721309542690323536a
AhmedArnaout123 <ahmed.arnaout123@gmail.com>
2020-09-29 15:33:27 +0300

    Create a data encapsulation model to remove repeatitve code

---
+++ lib/heap_sort/sorting_widget.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/sorting_bloc.dart';
+
+class InheritedSortingWidget extends InheritedWidget {
+  final UISortingDataProvider uiSortingDataProvider;
+
+  InheritedSortingWidget({@required this.uiSortingDataProvider});
+
+  static InheritedSortingWidget of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<InheritedSortingWidget>();
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
+  }
+}
+
+class SortingWidget extends StatefulWidget {
+  @override
+  _SortingWidgetState createState() => _SortingWidgetState();
+}
+
+class _SortingWidgetState extends State<StatefulWidget> {
+  UISortingDataProvider uiSortingDataProvider;
+  AnimationController _animationController;
+  Future<void> swipeItems(int index1, int index2) async {
+    RenderBox child1Box = uiSortingDataProvider
+        .itemsWidgetsGlobalKeys[index1].currentContext
+        .findRenderObject();
+    var child1Offset = child1Box.localToGlobal(Offset.zero);
+    RenderBox child2Box = uiSortingDataProvider
+        .itemsWidgetsGlobalKeys[index2].currentContext
+        .findRenderObject();
+    var child2Offset = child2Box.localToGlobal(Offset.zero);
+
+    double xDistance = child1Offset.dx - child2Offset.dx;
+    double yDistance = child1Offset.dy - child2Offset.dy;
+
+    double child1TotalDx = -xDistance;
+    double child2TotalDx = -child1TotalDx;
+
+    double child1TotalDy = -yDistance;
+    double child2TotalDy = -child1TotalDy;
+
+    Offset newChild1Offset = Offset(child1TotalDx, child1TotalDy);
+    Offset newChild2Offset = Offset(child2TotalDx, child2TotalDy);
+
+    Animation<Offset> index1Animation =
+        Tween<Offset>(begin: Offset.zero, end: newChild1Offset)
+            .animate(_animationController);
+    Animation<Offset> index2Animation =
+        Tween<Offset>(begin: Offset.zero, end: newChild2Offset)
+            .animate(_animationController);
+    void listner() {
+      setState(() {
+        uiSortingDataProvider.itemsWidgetsOffsets[index1] =
+            index1Animation.value;
+        uiSortingDataProvider.itemsWidgetsOffsets[index2] =
+            index2Animation.value;
+      });
+    }
+
+    _animationController.addListener(listner);
+
+    await _animationController.forward();
+    _animationController.removeListener(listner);
+    setState(() {
+      swipeItems(index1, index2);
+      uiSortingDataProvider.itemsWidgetsOffsets[index1] = Offset.zero;
+      uiSortingDataProvider.itemsWidgetsOffsets[index2] = Offset.zero;
+    });
+    _animationController.reset();
+  }
+
+  void showComparingIndicators(int index1, int index2) {
+    setState(() {
+      uiSortingDataProvider.haveItemsComparingIndicators[index1] = true;
+      uiSortingDataProvider.haveItemsComparingIndicators[index2] = true;
+    });
+  }
+
+  void hideComparingIndicators(int index1, int index2) {
+    setState(() {
+      uiSortingDataProvider.haveItemsComparingIndicators[index1] = false;
+      uiSortingDataProvider.haveItemsComparingIndicators[index2] = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SortingBloc, SortingState>(
+      listener: (context, state) {
+        if (state is SwapingItemsSortingState) {
+          swipeItems(state.index1, state.index2);
+          return;
+        }
+        if (state is ComparingItemsInProgressSortingState) {
+          showComparingIndicators(state.index1, state.index2);
+          return;
+        }
+        if (state is ComparingItemsIsDoneSortingState) {
+          hideComparingIndicators(state.index1, state.index2);
+        }
+      },
+      child:
+          InheritedSortingWidget(uiSortingDataProvider: uiSortingDataProvider),
+    );
+  }
+}
+
+class UISortingDataProvider {
+  List<int> items;
+  List<Offset> itemsWidgetsOffsets;
+  List<GlobalKey> itemsWidgetsGlobalKeys;
+  List<bool> haveItemsComparingIndicators;
+
+  UISortingDataProvider(
+      {@required this.items,
+      @required this.haveItemsComparingIndicators,
+      @required this.itemsWidgetsGlobalKeys,
+      @required this.itemsWidgetsOffsets});
+}
